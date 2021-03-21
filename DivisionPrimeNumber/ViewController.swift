8c500af542e527ae2b337775df3b7c235630785e
ryo.maemura <ryomaemura34@gmail.com>
2021-03-21 19:08:18 +0900

    add text-label

--- DivisionPrimeNumber/ViewController.swift
+++ DivisionPrimeNumber/ViewController.swift
@@ -8,7 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  @IBOutlet weak var primeNumberLabel: UILabel!
+  
+  @IBOutlet weak var number2Label: UIButton!
+  @IBOutlet weak var number3Label: UIButton!
+  @IBOutlet weak var number5Label: UIButton!
+  @IBOutlet weak var number7Label: UIButton!
+  @IBOutlet weak var number11Label: UIButton!
+  @IBOutlet weak var number13Label: UIButton!
+  @IBOutlet weak var number17Label: UIButton!
+  @IBOutlet weak var number19Label: UIButton!
+  @IBOutlet weak var number23Label: UIButton!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
