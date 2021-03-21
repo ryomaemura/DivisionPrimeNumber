1a9bcd9c3c57304ed8afc0e6b0024c47fe7c8533
ryo.maemura <ryomaemura34@gmail.com>
2021-03-21 15:53:23 +0900

    Initial Commit

---
+++ DivisionPrimeNumber/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DivisionPrimeNumber
+//
+//  Created by Maemura Ryo on 2021/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
